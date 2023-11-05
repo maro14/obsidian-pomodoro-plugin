eb23ac4bcd7234cb22ff951263987dca01c117b3
Teheskhiel Fritz <teheskhielfritz@gmail.com>
2023-11-05 08:48:12 +0700

    main testing

--- main.ts
+++ main.ts
@@ -6,54 +6,49 @@ interface PomodoroTimerSettings {
 	pomodoroLength: number;
 	shortBreakLength: number;
 	longBreakLength: number;
+	resetPomodoro: boolean;
+	endPomodoro: boolean;
 }
 
 const DEFAULT_SETTINGS: PomodoroTimerSettings = {
 	pomodoroLength: 25,
 	shortBreakLength: 5,
-	longBreakLength: 15
+	longBreakLength: 15,
+	resetPomodoro: false,
+	endPomodoro: false,
 }
 
 export default class PomodoroTimerPlugin extends Plugin {
 	settings: PomodoroTimerSettings;
+	public pomodoroLength: number;
+	public shortBreakLength: number;
+	public longBreakLength: number;
+
+	public pomodoroTImeout: null;
+
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon for pomodoro time.
-		const ribbonIconEl = this.addRibbonIcon('timer', 'Pomodoro Timer', () => {
-			new Notice('This is from pomodoro timer!');
+		const pomodoroIconEl = this.addRibbonIcon('clock', 'Pomodoro Timer', () => {
+			new Notice('The pomdomoro timer has been started!');
+			this.startPomodoro();
 		});
 
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		pomodoroIconEl.addClass('my-ribbon-icon');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-
 		this.addCommand({
 			id: 'start-pomodoro',
 			name: 'Start pomodoro',
 			callback: () => {
+				this.resetPomodoro()
 				this.startPomodoro();
 			}	
 		})
@@ -65,25 +60,14 @@ export default class PomodoroTimerPlugin extends Plugin {
 				this.startBreak();
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+		this.addCommand({
+			id: 'long-break',
+			name: 'Start long break',
+			callback: () => {
+				this.longBreak();
 			}
-		});
+		})
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new PomodoroSettingTab(this.app, this));
@@ -128,23 +112,32 @@ export default class PomodoroTimerPlugin extends Plugin {
 			this.startPomodoro();
 		}, this.settings.shortBreakLength * 60 * 1000);
 	}
+
+	resetPomodoro() {
+		new Notice('Pomodoro reset!');
+		this.settings.resetPomodoro = true;
+		setTimeout(() => {
+			this.settings.resetPomodoro = false;
+			this.startPomodoro();
+		}, 5000);
+	}
+
+	//create a function that will reset the long break timer
+	longBreak() {
+		new Notice('Long break started!');
+		setTimeout(() => {
+
+			new Notice('Long break ended!');
+			if (this.settings.resetPomodoro == true) {
+				this.resetPomodoro();
+			} else {
+				this.startPomodoro
+			}
+		}, this.settings.longBreakLength * 60 * 1000);
+	}
+
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
 
 class PomodoroSettingTab extends PluginSettingTab {
 	plugin: PomodoroTimerPlugin;
